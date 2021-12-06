938ddb6cac52e7fdef7deb2218dc65c87347d7e5
Valeriy Sagatovskiy <sagatovskiyva@m2.ru>
2021-12-06 23:08:19 +0300

    chore(lerna): publish setup

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    parserOptions: {
+        sourceType: 'module',
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+    plugins: ['react-hooks', 'import'],
+    rules: {
+        'react/display-name': 'off',
+        'react/prop-types': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'import/order': [
+            'error',
+            {
+                groups: [['external', 'internal']],
+                'newlines-between': 'always',
+            },
+        ],
+        'react/self-closing-comp': 'error',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
