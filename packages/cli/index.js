4cb8b236d6f10c422d24593fefbe6e7587fc5259
Valeriy Sagatovskiy <sagatovskiyva@m2.ru>
2021-12-07 14:33:45 +0300

    feat(cli): created cli script for project biolerplate

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,8 +1,48 @@
 #! /usr/bin/env node
 
-// const { wir } = require('fs');
+const eslintPkg = require('@vsgt/eslint-config/package.json');
+const prettierPkg = require('@vsgt/prettier-config/package.json');
+
+const { promises: fs } = require('fs');
 const path = require('path');
+const targetPkgPath = path.resolve(process.cwd(), 'package.json');
 
-const pkg = path.resolve(process.cwd(), 'package.json');
+const targetPkg = require(targetPkgPath);
 
-console.log(pkg);
+const gitignore = path.resolve(__dirname, 'dist/.gitignore');
+const eslintignore = path.resolve(__dirname, 'dist/.eslintignore');
+
+const linters = {
+    prettier: '@vsgt/prettier-config',
+    eslintConfig: {
+        extends: '@vsgt/eslint-config',
+    },
+};
+
+const deps = {
+    eslint: eslintPkg.peerDependencies.eslint.replace('^', ''),
+    prettier: prettierPkg.peerDependencies.prettier.replace('^', ''),
+    ['@vsgt/prettier-config']: prettierPkg.version,
+    ['@vsgt/eslint-config']: eslintPkg.version,
+};
+
+const modifiedPkg = {
+    ...targetPkg,
+    ...linters,
+    devDependencies: {
+        ...(targetPkg.devDependencies || {}),
+        ...deps,
+    },
+};
+
+async function setup() {
+    await Promise.all([
+        fs.copyFile(gitignore, path.resolve(process.cwd(), './.gitignore')),
+        fs.copyFile(eslintignore, path.resolve(process.cwd(), './.eslintignore')),
+        fs.writeFile(targetPkgPath, JSON.stringify(modifiedPkg, null, 4), { encoding: 'utf8', flag: 'w' }),
+    ]);
+
+    console.log('Success!');
+}
+
+setup();
